6518d9589fa2d8928f6b892df6942e3c4fbde7f5
norman <normanmukama11@gmail.com>
2023-11-02 22:41:07 +0300

    have made changes in index.html file

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*===================typing animation ==========*/
+var typed = new Typed(".typing", {
+    strings:["Web Designer", "Web Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    BackSpeed: 80,
+    loop: true
+})
