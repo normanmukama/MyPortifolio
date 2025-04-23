1d063f4772b3f18f98631e39dd7f7a39da4762a9
normanmukama <normanmukama11@gmail.com>
2025-04-23 19:50:29 +0300

    Text animation

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/*===================typing animation ==========*/
-var typed = new Typed(".typing", {
-    strings:["Web Designer", "TALL stack developer", "Database system manager"],
-    typeSpeed: 100,
-    BackSpeed: 80,
-    loop: true
-})
